92663b291afd9187d659e98fb98b506c3bdbb61d
lucasbrun196 <luucaasbrum13@gmail.com>
2024-05-18 19:00:45 -0300

    doc

--- index.cpp
+++ index.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main(){
+    ifstream arq;
+    string t;
+    arq.open("lorem.txt");
     
+    if(!arq){
+        cout << "Erro ao abrir arquivo!\n";
+    }
     return 0;
 }
