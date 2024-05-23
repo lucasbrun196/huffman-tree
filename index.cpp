2cf3159c14335a319ebe19f2563fe8fc8f4c8f39
lucasbrun196 <luucaasbrum13@gmail.com>
2024-05-23 00:47:01 -0300

    frequency map

--- index.cpp
+++ index.cpp
@@ -1,15 +1,37 @@
 #include <iostream>
 #include <fstream>
+#include <map>
 
 using namespace std;
 
+bool charSearch(const char& ch, const map<char, int>& frequency_map) {
+    return frequency_map.find(ch) != frequency_map.end();
+}
+
 int main(){
     ifstream arq;
-    string t;
-    arq.open("lorem.txt");
+    char ch;
+    int count_char;
+    map<char, int> frequency_map;
+
+    arq.open("./txt/lorem.txt");
     
     if(!arq){
         cout << "Erro ao abrir arquivo!\n";
+    }
+
+    
+    while(arq.get(ch)){
+        if(charSearch(ch, frequency_map)){
+            frequency_map[ch]++;
+        }else{
+            frequency_map[ch] = 1;
+        }
+    }
+    
+
+    for(auto conj: frequency_map){
+        cout << conj.first << " - " << conj.second << endl; 
     }
     return 0;
 }
