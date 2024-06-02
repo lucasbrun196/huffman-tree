7bad44fd26b97ca916d3d5092f59cb03a695c919
lucasbrun196 <luucaasbrum13@gmail.com>
2024-06-02 16:53:23 -0300

    sorted linked list

--- index.cpp
+++ index.cpp
@@ -4,15 +4,59 @@
 
 using namespace std;
 
-bool charSearch(const char& ch, const map<char, int>& frequency_map) {
+struct Node{
+    int frequency;
+    char word;
+    Node *next;
+};
+
+
+bool charSearch(const char &ch, const map<char, int> &frequency_map) {
     return frequency_map.find(ch) != frequency_map.end();
 }
+
+void recursiveInsertToLinkedList(Node *&nodeFirst, Node *&nodeToInsert){
+    if(!nodeFirst->next){
+        nodeFirst->next = nodeToInsert;
+        return;
+    }
+    if(nodeFirst->next->frequency > nodeToInsert->frequency){
+        nodeToInsert->next = nodeFirst->next;
+        nodeFirst->next = nodeToInsert;
+        return;
+    }else{
+        recursiveInsertToLinkedList(nodeFirst->next, nodeToInsert);
+    }
+}
+
+void sortedLinkedList(const char wd, const int fqc, Node *&nodeFirst){
+    Node *node = new Node;
+    node->frequency = fqc;
+    node->word = wd;
+    if(node->frequency < nodeFirst->frequency){
+        node->next = nodeFirst;
+        nodeFirst = node;
+    }else{
+        recursiveInsertToLinkedList(nodeFirst, node);
+    }
+}
+
+//just to teste:
+void showLinkedList(Node *first){
+    if(!first) return;
+    cout << first->frequency << " - ";
+    cout << first->word << endl;
+    showLinkedList(first->next);
+}
+
+
 
 int main(){
     ifstream arq;
     char ch;
     int count_char;
     map<char, int> frequency_map;
+    
 
     arq.open("./txt/lorem.txt");
     
@@ -29,9 +73,25 @@ int main(){
         }
     }
     
-
-    for(auto conj: frequency_map){
-        cout << conj.first << " - " << conj.second << endl; 
+    
+    
+    Node *nodeFirst = new Node;
+    int contAux = 0;
+    for(auto conj : frequency_map){
+        if(contAux == 0){
+            nodeFirst->word = conj.first;
+            nodeFirst->frequency = conj.second;
+            nodeFirst->next = nullptr;
+        }
+        else{
+            sortedLinkedList(conj.first, conj.second, nodeFirst);
+        }
+        contAux++;
     }
+
+    //just to teste:
+    showLinkedList(nodeFirst);
+    
+
     return 0;
 }
